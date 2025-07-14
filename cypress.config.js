4e7db9688fa2896cd077062ecb7eb03c4597b711
Ivan Ferrer <ivanferrer@Ivans-MacBook-Pro.local>
2025-07-13 23:05:58 -0600

    Use relative path for protocol tests - works in any environment

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,11 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js',  // ✅ Changed from commands.js to e2e.js
+    supportFile: 'cypress/support/e2e.js',
     baseUrl: 'https://live.blacklinesafety.com',
     setupNodeEvents(on, config) {},
     env: {
-      protocolPath: 'http://127.0.0.1:5501/automated-basic-non-gas-alert-protocol/index.html'
+      protocolPath: '../automated-basic-non-gas-alert-protocol/index.html'
     }
   },
 });
